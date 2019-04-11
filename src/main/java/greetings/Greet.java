d3dc191ee1ddf3d47972b6c7ef6196e012c3178b
Mfundo Ngqanekana <mfundo8934@gmail.com>
2019-04-11 11:44:43 +0200

    added greeted command

--- src/main/java/greetings/Greet.java
+++ src/main/java/greetings/Greet.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Greet{
 
     public static int Counter = 0;
+    private static String name;
+
+
 
     public static void main(String[]args){
 
@@ -14,16 +17,46 @@ public class Greet{
 
         String command;
 
+        System.out.println("=======================WELCOME TO MY GREETINGS APP ============================= \n \n");
         while (Counter >= 0){
 
-            System.out.println("Enter a command");
+            System.out.println("Enter a command  or Enter the keyword help to see all possible commands");
             command = greeting.nextLine();
 
             String[] comm = command.split(" ");
 
             if(comm[0].equalsIgnoreCase("greet")){
-                String msg = greeter.greeting(comm[1], comm[2]);
-                System.out.println(msg);
+                if(comm.length == 3){
+                    String msg = greeter.greeting(comm[1], comm[2]);
+                    System.out.println(msg);
+                }else {
+                    String msg = greeter.greeting(comm[1], "English");
+                    System.out.println(msg);
+                }
+            }
+            else if(comm[0].equalsIgnoreCase("greeted")){
+
+
+                name = null;
+                if(comm.length == 2) {
+                    name = comm[1];
+                }
+
+                    String msg = (String) greeter.greeted(name);
+                    System.out.println(msg);
+
+            }
+            else if(comm[0].equalsIgnoreCase("clear")){
+                if(comm.length  == 2){
+                    name = comm[1];
+                    greeter.clearUser(name);
+                }
+                else {
+                    greeter.clearAllGreetings();
+                }
+                }
+            else if(comm[0].equalsIgnoreCase("exit")){
+                break;
             }
             else {
                 System.out.println("Invalid command!");
--- src/main/java/greetings/Greeter.java
+++ src/main/java/greetings/Greeter.java
@@ -5,8 +5,41 @@ import java.util.HashMap;
 public class Greeter {
 
     HashMap<String, Integer> allGreetings = new HashMap<String, Integer>();
+
     public String greeting(String name, String language){
+        if (allGreetings.containsKey(name)) {
+            allGreetings.put(name, allGreetings.get(name) + 1);
+        } else {
+            allGreetings.put(name, 1);
+        }
         name = name.substring(0,1).toUpperCase() +  name.substring(1).toLowerCase();
         return Languages.valueOf(language).getGreets() + " " + name;
     }
+
+    public String greeted(String name){
+        try {
+            if (name == null){
+                System.out.println("======People who have been greeted and the number of greets per person========\n");
+                return allGreetings.toString();
+            }
+            else{
+                System.out.println("======  " + name + " has been greeted ==========\n");
+                return allGreetings.get(name).toString();
+            }
+        }
+        catch (NullPointerException e){
+            return allGreetings.toString();
+        }
+    }
+
+    public void clearAllGreetings(){
+        allGreetings.clear();
+    }
+
+    public void clearUser(String name){
+       allGreetings.remove(name);
+    }
+
+
+
 }
