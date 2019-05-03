14f0fa39755b05dc9a6bce25d9e26ae7eb081b72
vivo8934 <mfundo8934@gmail.com>
2019-05-03 15:50:36 +0200

    connecting db

--- src/test/java/greetings/JdbcTest.java
+++ src/test/java/greetings/JdbcTest.java
@@ -1,9 +1,13 @@
 package greetings;
 
 
+import org.junit.jupiter.api.Test;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class JdbcTest {
@@ -13,12 +17,36 @@ public class JdbcTest {
 
     final String DATABASE_URL = "jdbc:h2:./target/greetings-console";
 
+
+
     public Connection getConnection() throws Exception {
         Class.forName("org.h2.Driver");
 
         Connection conn;
         conn = DriverManager.getConnection(DATABASE_URL,USER,PASS);
         return conn;
+    }
+    @Test
+    public void LoadingDriver(){
+        try {
+            Class.forName("org.h2.Driver");
+        }catch (ClassNotFoundException e){
+            fail(e);
+        }
+    }
+
+    @Test
+    public void ConnectingToDb(){
+        try {
+            Class.forName("org.h2.Driver");
+
+            Connection conn = DriverManager.getConnection(DATABASE_URL,USER,PASS);
+            System.out.println(conn);
+        } catch (ClassNotFoundException e){
+            fail(e);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 
